f22efe2ed516c8083346b5320f4aa1aff957844a
Hermano Lira <hermanolira@gmail.com>
2017-02-17 14:08:21 -0300

    Adição JPA

--- src/main/java/br/com/hal/livraria/bean/LivroBean.java
+++ src/main/java/br/com/hal/livraria/bean/LivroBean.java
@@ -2,10 +2,18 @@ package br.com.hal.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 
+import br.com.hal.livraria.model.Livro;
+
 @ManagedBean
 public class LivroBean {
 
+	private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
+	}
+
 	public void gravar() {
-		System.out.println("Gravando Livro");
+		System.out.println("Gravando Livro " + getLivro().getTitulo());
 	}
 }
